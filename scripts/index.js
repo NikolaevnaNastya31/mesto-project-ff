1c6315b2b37dab6c0325d50fe6bc123f398ada4f
NikolaevnaNastya31 <nikolaevnavrostove31@yandex.ru>
2024-10-24 16:35:01 +0300

    first

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,32 @@
-// @todo: Темплейт карточки
+// // @todo: Темплейт карточки
+const cardTemplate =  document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
+const placeList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
+ function addCard(cardData){
+     const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+     
+     cardElement.querySelector('.card__title').textContent = cardData.name;
+     cardElement.querySelector('.card__image').src = cardData.link;
+     cardElement.querySelector('.card__image').alt = cardData.name;
 
+    const deleteButton = cardElement.querySelector('.card__delete-button');  
+     deleteButton.addEventListener('click', function () {
+         cardElement.remove()
+     });     
+        
+    return cardElement;
+}
 // @todo: Функция удаления карточки
+ function deleteCard(cardElement){
+     cardElement.remove();
+ }
 
 // @todo: Вывести карточки на страницу
+
+initialCards.forEach(function (i) {
+    const cardList = addCard(i);
+    placeList.append(cardList);
+})
